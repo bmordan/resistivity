47febadd8f1fe3080042e5e66ea182d4a100538a
Bernard Mordan <bernardmordan@gmail.com>
2014-05-10 21:48:45 +0100

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Resitivity::Application.config.secret_key_base = 'ac4124aca7067ce35b35135a6697d88169f853ae21c14fe29f982bd1d4fd3f359645f947aa15acd958d7bd4f0d41f3ab988098a4770fa907ed4eb58a4968f29b'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Resitivity::Application.config.session_store :cookie_store, key: '_resitivity_session'
