4328ad2dc0f7467b31e3763313e051e5d713635f
Bernard Mordan <bernardmordan@gmail.com>
2014-05-11 13:39:39 +0100

    initial user module forming

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,5 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+User.create( username: 'bernie', email: 'bernardmordan@gmail.com', encrypted_password: '', salt: '' )
