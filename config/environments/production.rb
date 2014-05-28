208c087aa606e4c32f578163e62552e737603210
Bernard Mordan <bernardmordan@gmail.com>
2014-05-28 16:16:51 +0100

    changed Auth:: to Resistivity:: in config/environments/production.rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,4 +1,4 @@
-Auth::Application.configure do
+Resistivity::Application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
 
   # Code is not reloaded between requests.
