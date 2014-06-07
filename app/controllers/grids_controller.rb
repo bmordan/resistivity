a205d8c32052439b267bd7f6085497ad1dbd2fa8
Bernard Mordan <bernardmordan@gmail.com>
2014-06-07 16:31:56 +0100

    update bug cleaned out, individual rownumbers stored in db

--- app/controllers/grids_controller.rb
+++ app/controllers/grids_controller.rb
@@ -7,14 +7,15 @@ class GridsController < ApplicationController
 
   def create
     @row = ''
-    params.each do | k , v |
+    @rownumber = 0
+    params.each do |k,v|
       unless ['utf8','authenticity_token','survey_id','grid_x'].include? k then
         v == "" ? v = 0 : v = v.to_s
         if k.split("|")[1].to_i < params[:grid_x].to_i
           @row << v.to_s+","
         else
           @row << v.to_s
-          @grid = Grid.new({"row"=>@row,"rownumber"=>params[:grid_x],"survey_id"=>params[:survey_id]})
+          @grid = Grid.new({"row"=>@row,"rownumber"=>@rownumber+=1,"survey_id"=>params[:survey_id]})
           @grid.save
           @row = ''
         end         
@@ -29,22 +30,32 @@ class GridsController < ApplicationController
   end
   
   def update
-    Grid.where(:survey_id => params[:survey_id]).destroy_all
+    @hash = {}
+    
+    @cols = []
+    params.each do |k,v|
+      if k.include? "|"
+        @cols << k.split("|")[1].to_i
+      end 
+    end
+    @cols.max()
+    
     @row = ''
-    params.each do | k , v |
-      unless ['utf8','authenticity_token','_method','action','controller','survey_id','grid_x','id'].include? k then
-        v == "" ? v = 0 : v = v.to_s
-        if k.split("|")[1].to_i < params[:grid_x].to_i
+    params.each do |k,v|
+      if k.include? "|"
+        if k.split("|")[1].to_i < @cols.max()
           @row << v.to_s+","
         else
           @row << v.to_s
-          @grid = Grid.new({"row"=>@row,"rownumber"=>params[:grid_x],"survey_id"=>params[:survey_id]})
+          @grid = Grid.find_by_id(k.split("|")[0])
+          @grid.row = @row
           @grid.save
           @row = ''
-        end         
+        end  
       end
     end
-    redirect_to survey_path(params[:survey_id]) 
+    
+ #   redirect_to survey_path(params[:survey_id]) 
   end
 
   private
