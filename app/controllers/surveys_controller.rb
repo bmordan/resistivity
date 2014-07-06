1819d319a5d4949939fd3812ea53a45f1c54bbfa
Bernard Mordan <bernardmordan@gmail.com>
2014-07-06 10:27:54 +0100

    roll back to session[:user_id]

--- app/controllers/surveys_controller.rb
+++ app/controllers/surveys_controller.rb
@@ -5,7 +5,7 @@ class SurveysController < ApplicationController
        redirect_to login_path
      else
         @user = User.find(session[:user_id])
-        @surveys = Survey.find_by user_id(2) 
+        @surveys = Survey.find_by user_id(session[:user_id]) 
      end 
   end  
   
